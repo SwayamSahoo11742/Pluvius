14b07701bf9b8f401d761d340a8903bf98a4cb13
Dodo11742 <kabitamsahoo@gmail.com>
2023-02-15 22:21:50 -0500

    clearing

--- scopul_exception.py
+++
@@ -1,8 +0,0 @@
-class InvalidFileFormatError(Exception):
-    def __init__(self, value) -> None:
-        self.value = value
-
-
-class InvalidMusicElementError(Exception):
-    def __init__(self, value) -> None:
-        self.value = value
